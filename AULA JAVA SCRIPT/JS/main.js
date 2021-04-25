d9866673fcaf40d0606c49fdea50f3061796533d
cartelifabricio <cartelifabricio@gmail.comm>
2021-04-25 03:02:35 -0300

    novas aulas

--- AULA JAVA SCRIPT/JS/main.js
+++ AULA JAVA SCRIPT/JS/main.js
@@ -1,1 +1,55 @@
-alert("meu primeiro JS");
+
+
+/* Exercicio 1
+var nome = "Fabricio Carteli";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+
+//alert(nome + " tem " + idade + " anos ");
+
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+*/
+
+/* Exercicio 2
+var lista = ["maca", "pera", "laranja"] ;
+lista.push("uva");
+
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+/* Exercicio 3
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/* Exercicio 4
+var fruta = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor: "roxo"}]
+console.log(fruta);
+alert(fruta[1].nome);
+*/
+
+/* Exercicio 5
+var idade = prompt("Qual sua idade?");
+
+if (idade >=18) {
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/  
+
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+}
+
